249558f0d8777db7a113f3483c84ee8af218b667
Anjalee <anee.anjalee42@gmail.com>
2025-09-13 11:39:39 +0530

    Commit for Service and repository layer Structure

--- Autosuggest/src/main/java/com/example/autosuggest/repository/WordRepository.java
+++ Autosuggest/src/main/java/com/example/autosuggest/repository/WordRepository.java
@@ -1,9 +1,14 @@
 package com.example.autosuggest.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.autosuggest.model.Word;
 
+@Repository
 public interface WordRepository extends JpaRepository<Word,Long>{
+	Optional<Word> findByText(String text);
 
 }
--- Autosuggest/src/main/java/com/example/autosuggest/service/WordService.java
+++ Autosuggest/src/main/java/com/example/autosuggest/service/WordService.java
@@ -1,5 +1,37 @@
 package com.example.autosuggest.service;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.example.autosuggest.model.Word;
+
+@Service
 public class WordService {
+	public WordService() {}
+	
+	/**
+	 * Add a new word to DB, Trie and Cache
+	 */
+	public Word addWord(String text) {
+		return null;
+	}
+	
+	/**
+	 * Get word from Cache -> DB (if not present)
+	 * Updates cache and trie
+	 */
+	
+	public Word getWord(String text) {
+		return null;
+	}
+	
+	/**
+	 * Get auto-suggestions from Trie
+	 */
+	
+	public List<String> getSuggestions(String prefix){
+		return null;
+	}
 
 }
